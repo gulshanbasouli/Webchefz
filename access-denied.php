366008591d7fe0140844b0d4cfd749a0bb79ace3
gulshanbasouli <32733940+gulshanbasouli@users.noreply.github.com>
2017-10-18 11:23:12 +0530

    Update access-denied.php
    Code is updated

--- access-denied.php
+++ access-denied.php
@@ -1,2 +1,36 @@
-# Webchefz
-Wordpress Admin access denied for subscribers
+<?php
+/**
+*Plugin Name: Minigiv Dashboard Access Denied
+*Author: Webchefz
+*Author URI: https://www.webchefz.com
+*Description: Dashboard Access Denied for users
+*Plugin URI:  https://github.com/gulshanbasouli/Webchefz
+Version: 0.1
+*/
+
+
+
+// Remove Admin access for subscribers
+
+    /**
+ * Disable admin bar for subscribers and who cannot edit posts
+ * for subscribers.
+ */
+function themeblvd_disable_admin_bar() {
+    if ( ! current_user_can('edit_posts') ) {
+        add_filter('show_admin_bar', '__return_false');
+    }
+}
+add_action( 'after_setup_theme', 'themeblvd_disable_admin_bar' );
+ 
+/**
+ * Redirect back to homepage and not allow access to
+ * WP admin for Subscribers.
+ */
+function themeblvd_redirect_admin(){
+    if ( ! defined('DOING_AJAX') && ! current_user_can('edit_posts') && DOING_AJAX) {
+        wp_redirect( site_url() );
+        exit;      
+    }
+}
+add_action( 'admin_init', 'themeblvd_redirect_admin' );
